24a6a3a34555b6100e78ed43b643b1346b8c4aec
Khaerul Umam <khaerul.umam@digitalsekuriti.id>
2023-11-11 20:31:21 +0700

    uuuuuyeeahhh

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,48 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+<script lang="ts">
+	import Navbar from "./sverdle/navbar.svelte";
+
+
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Khairul Umam</title>
+	<meta name="Khairul Umam" content="About Me..." />
 </svelte:head>
+<style>
+	h1 {
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  display: inline-block;
+  margin: 0 auto;
+  font-weight: 900;
+  text-transform: none;
+  text-align: center;
+  font-size: 6vw;
+  animation: background 20s linear infinite;
+  
+  background: linear-gradient(90deg, rgba(150,48,30,1) 0%,rgba(199,201,88,1) 10%,rgba(28,147,46,1) 20%,rgba(74,165,168,1) 30%,rgba(89,90,165,1) 40%,rgba(84,16,67,1) 50%,rgba(28,58,63,1) 60%,rgba(82,175,183,1) 70%,rgba(78,170,76,1) 80%,rgba(150,48,30,1) 90%,rgba(199,201,88,1) 100%);
+  background-size: 1000% 100%;
+  
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  
+}
 
-<section>
-	<h2 class="text-red-500">
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-</section>
+
+
+@keyframes background {
+  0%{background-position:0% 50%}
+  100%{background-position:100% 50%}
+}
+</style>
+
+
+<div >
+	<div class="flex items-center justify-center mt-12">
+		<Navbar />
+	</div>
+	<div class="h-96 flex flex-col items-center justify-center">
+	<h1>Hi. I'm Khairul.</h1>
+	<h1>a Software Engineer</h1>
+</div>
+
+</div>
---
+++ src/routes/sverdle/navbar.svelte
@@ -0,0 +1,3 @@
+<div class="w-96 h-14 bg-white/20 backdrop-blur-lg rounded-full">
+
+</div>
